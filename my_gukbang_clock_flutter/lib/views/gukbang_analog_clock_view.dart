dae20604b078888cb8c790261ff640e4ad349e92
Yang Sijun <sijun0905@gmail.com>
2023-12-17 23:44:10 +0900

    Update on gukbang analog clock

--- my_gukbang_clock_flutter/lib/views/gukbang_analog_clock_view.dart
+++ my_gukbang_clock_flutter/lib/views/gukbang_analog_clock_view.dart
@@ -20,8 +20,8 @@ class _GukbangAnalogClockViewState extends State<GukbangAnalogClockView> {
       builder: ((context, snapshot) {
         if (snapshot.hasData) {
           return SizedBox(
-            width: 400,
-            height: 400,
+            width: 320,
+            height: 320,
             child: CustomPaint(
               painter: GukbangAnalogClockPainter(
                 dateTime: snapshot.data!,
@@ -50,41 +50,43 @@ class GukbangAnalogClockPainter extends CustomPainter {
     final centerY = size.height / 2;
     final center = Offset(centerX, centerY);
 
-    const radius = 200.0;
-
+    const radius = 160.0;
     final fillBrush = Paint()..color = const Color(0xFF444974);
-
     final outlineBrush = Paint()
       ..color = const Color(0xFFEAECFF)
       ..style = PaintingStyle.stroke
       ..strokeWidth = 16;
+    canvas.drawCircle(center, radius, fillBrush);
+    canvas.drawCircle(center, radius, outlineBrush);
 
-    canvas.drawCircle(center, radius - 40, fillBrush);
-
-    canvas.drawCircle(center, radius - 40, outlineBrush);
+    final scaleBrush = Paint()
+      ..color = const Color(0xFFEAECFF)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4;
+    for (int i = 0; i < 360; i += 30) {
+      final x1 = centerX + (radius - 20) * cos(i * pi / 180 - pi / 2);
+      final y1 = centerY + (radius - 20) * sin(i * pi / 180 - pi / 2);
+      final x2 = centerX + radius * cos(i * pi / 180 - pi / 2);
+      final y2 = centerY + radius * sin(i * pi / 180 - pi / 2);
+      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), scaleBrush);
+    }
 
     final hourHandBrush = Paint()
       ..shader = const RadialGradient(colors: [Color(0xFF748EF6), Color(0xFF77DDFF)]).createShader(Rect.fromCircle(center: center, radius: radius))
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
       ..strokeWidth = 8;
-
-    final hourHandX = centerX + 60 * cos(dateTime.hour * 30 * pi / 180 - pi / 2);
-
-    final hourHandY = centerY + 60 * sin(dateTime.hour * 30 * pi / 180 - pi / 2);
-
+    final hourHandX = centerX + 70 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180 - pi / 2);
+    final hourHandY = centerY + 70 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180 - pi / 2);
     canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHandBrush);
 
     final minutesHandBrush = Paint()
       ..shader = const RadialGradient(colors: [Color(0xFFEA74AB), Color(0xFFC279FB)]).createShader(Rect.fromCircle(center: center, radius: radius))
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 6;
-
-    final minutesHandX = centerX + 80 * cos(dateTime.minute * 6 * pi / 180 - pi / 2);
-
-    final minutesHandY = centerY + 80 * sin(dateTime.minute * 6 * pi / 180 - pi / 2);
-
+      ..strokeWidth = 8;
+    final minutesHandX = centerX + 100 * cos((dateTime.minute * 6 + dateTime.second * 0.1) * pi / 180 - pi / 2);
+    final minutesHandY = centerY + 100 * sin((dateTime.minute * 6 + dateTime.second * 0.1) * pi / 180 - pi / 2);
     canvas.drawLine(center, Offset(minutesHandX, minutesHandY), minutesHandBrush);
 
     final secondsHandBrush = Paint()
@@ -92,16 +94,14 @@ class GukbangAnalogClockPainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.butt
       ..strokeWidth = 4;
-
-    final secondsHandX = centerX + 80 * cos(dateTime.second * 6 * pi / 180 - pi / 2);
-
-    final secondsHandY = centerY + 80 * sin(dateTime.second * 6 * pi / 180 - pi / 2);
-
+    final secondsHandX = centerX + 120 * cos(dateTime.second * 6 * pi / 180 - pi / 2);
+    final secondsHandY = centerY + 120 * sin(dateTime.second * 6 * pi / 180 - pi / 2);
     final secondsHandX2 = centerX - 20 * cos((dateTime.second + percent) * 6 * pi / 180 - pi / 2);
-
     final secondsHandY2 = centerY - 20 * sin((dateTime.second + percent) * 6 * pi / 180 - pi / 2);
-
     canvas.drawLine(Offset(secondsHandX, secondsHandY), Offset(secondsHandX2, secondsHandY2), secondsHandBrush);
+
+    final centerFillBrush = Paint()..color = const Color(0xFFEAECFF);
+    canvas.drawCircle(center, 4, centerFillBrush);
   }
   
   @override
