ecb724c3a913626a0992aa9ad52a7491fa3fd44f
Yang Sijun <sijun0905@gmail.com>
2023-12-18 00:35:43 +0900

    Update Title

--- my_gukbang_clock_flutter/lib/main.dart
+++ my_gukbang_clock_flutter/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Clock Demo',
+      title: '양시준의 국방시계',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
